7fa861a549376c26ea96f32ce39655a21c149e4f
Augustė Bagdzevičiūtė <79446863+augustebag@users.noreply.github.com>
2021-07-29 23:07:47 +0300

    lenteles

--- resources/views/better/index.blade.php
+++ resources/views/better/index.blade.php
@@ -1,95 +1,89 @@
 @extends('layouts.app')
 @section('content')
+
 <div class="container">
-   <div class="row justify-content-center">
-       <div class="col-md-8">
-           <div class="card">
-               <div class="card-header">
-                 <h2>Betters</h2>
+  <div class="row justify-content-center">
+    <div class="col-md-10">
+      <div class="card">
+        <div class="card-header">
+          <h2>Betters</h2>
 
-               <form action="{{route('better.index')}}" method="get" class="sort-form">
-                <fieldset>
-                    <legend>Sort by: </legend>
-                    <div>
-                      <label>Name </label>
-                      <input type="radio" name="sort_by" value="name" @if('name'== $sort) checked @endif>
-                    </div>
-                    <div>
-                      <label>Surname </label>
-                      <input type="radio" name="sort_by" value="surname" @if('surname'== $sort) checked @endif>
-                    </div>
-                </fieldset>
-                <fieldset>
-                    <legend>Direction: </legend>
-                    <div>
-                      <label>Up </label>
-                      <input type="radio" name="dir" value="asc" @if('asc'== $dir) checked @endif>
-                    </div>
-                    <div>
-                      <label>Down </label>
-                      <input type="radio" name="dir" value="desc" @if('desc'== $dir) checked @endif>
-                    </div>
-                </fieldset>
-                <button type="submit" class="btn btn-outline-dark btn-sm">Sort</button>
-                <a href="{{route('better.index')}}" class="btn btn-outline-danger btn-sm">Clear</a>
-            </form>
-
-              <form action="{{route('better.index')}}" method="get" class="sort-form">
-                <fieldset>
-                    <legend>Filter by: </legend>
-                    <div class="form-group">
-                        <select class="horse_id" class="form-control">
-                            @foreach($horses as $horse)
-                            <option value="{{$horse->id}}" @if($defaultHorse == $horse->id) selected @endif>
-                              {{$horse->name}} 
-                            </option>
-                            @endforeach
-                        </select>
-                        <small class="form-text text-muted">Select Better from the list.</small>
-                    </div>
-                </fieldset>
-                <button type="submit" class="btn btn-outline-dark btn-sm">Filter</button>
-                <a href="{{route('better.index')}}" class="btn btn-outline-danger btn-sm">Clear</a>
-            </form>
-            <form action="{{route('better.index')}}" method="get" class="sort-form">
-              <fieldset>
-                  <legend>Search: </legend>
-                  <div class="form-group">
-                      <input type="search" class="form-control mr-sm-2" placeholder="Search" aria-label="Search" name="s">
-                  </div>
-              </fieldset>
-              <button class="btn btn-sm btn-outline-secondary my-2 my-sm-0" type="submit">Search</button>
-              <a href="{{route('better.index')}}" class="btn btn-sm btn-outline-danger my-2 my-sm-0">Clear</a>
-          </form>
-            </div>
-
-               @foreach ($betters as $better)
-               <div class="card-body">
-                <ul class="list-group">
-                  <li class="list-group-item">
-                    <div class="list-container">
-                        <div class="list-container__content">
-                          <span class="list-container__content__better">{{$better->name}} {{$better->surname}} <br></span>
-                            <span class="list-container__content__horse"> Horse: {{$better->betterHorse->name}}<br> Bet: {{$better->bet}}</span>
-                        </div>
-                        <div class="list-container__buttons">
-                          <form method="POST" action="{{route('better.destroy', [$better])}}">
-                            @csrf
-                            <a href="{{route('better.show',[$better])}}" class="btn btn-outline-dark btn-sm">More info</a>
-                            <a href="{{route('better.edit',[$better])}}" class="btn btn-outline-dark btn-sm">Edit</a>
-                            <button type="submit" class="btn btn-outline-danger btn-sm">DELETE</button>
-                          </form>
-                        </div>
-                        </div>
-                    </div>
-                  </li>
-                  @endforeach
-                  {{$betters->links()}}
-                </ul>
-                </div>
+          <form action="{{route('better.index')}}" method="get" class="sort-form">
+            <fieldset>
+              <legend>Sort by: </legend>
+              <div>
+                <label>Name </label>
+                <input type="radio" name="sort_by" value="name" @if('name'==$sort) checked @endif>
               </div>
-          </div>
+              <div>
+                <label>Surname </label>
+                <input type="radio" name="sort_by" value="surname" @if('surname'==$sort) checked @endif>
+              </div>
+            </fieldset>
+            <button type="submit" class="btn btn-outline-dark btn-sm">Sort</button>
+            <a href="{{route('better.index')}}" class="btn btn-outline-danger btn-sm">Clear</a>
+
+            <fieldset>
+              <legend>Filter by: </legend>
+              <div class="form-group">
+                <select class="horse_id" class="form-control">
+                  @foreach($horses as $horse)
+                  <option value="{{$horse->id}}" @if($defaultHorse==$horse->id) selected @endif>
+                    {{$horse->name}}
+                  </option>
+                  @endforeach
+                </select>
+                <small class="form-text text-muted">Select Better from the list.</small>
+              </div>
+            </fieldset>
+            <button type="submit" class="btn btn-outline-dark btn-sm">Filter</button>
+          </form>
+          <form action="{{route('better.index')}}" method="get" class="sort-form">
+            <fieldset>
+              <legend>Search: </legend>
+              <div class="form-group">
+                <input type="search" class="form-control mr-sm-2" placeholder="Search" aria-label="Search" name="s">
+              </div>
+            </fieldset>
+            <button class="btn btn-sm btn-outline-secondary my-2 my-sm-0" type="submit">Search</button>
+            <a href="{{route('better.index')}}" class="btn btn-sm btn-outline-danger my-2 my-sm-0">Clear</a>
+          </form>
+        </div>
       </div>
-    </div>
-  @endsection
-  @section('title') Betters @endsection
+
+        
+        <table class="table table-striped table-dark">
+          <thead>
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Surname</th>
+              <th scope="col">Horse Name</th>
+              <th scope="col">Bet</th>
+              <th scope="col">*</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach ($betters as $better) 
+            <tr>
+                  <td>{{$better->name}}</td>
+                  <td>{{$better->surname}}</td>
+                  <td>{{$better->betterHorse->name}}</td>
+                  <td>{{$better->bet}}</td>
+                  <td class="list-container__buttons">
+                    <form method="POST" action="{{route('better.destroy', [$better])}}">
+                      @csrf
+                      <a href="{{route('better.show',[$better])}}" class="btn btn-outline-dark btn-sm">More info</a>
+                      <a href="{{route('better.edit',[$better])}}" class="btn btn-outline-dark btn-sm">Edit</a>
+                      <button type="submit" class="btn btn-outline-danger btn-sm">DELETE</button>
+                    </form>
+                  </td>
+                </tr>
+                @endforeach
+                </tbody>
+              </table>
+    {{$betters->links()}}
+  </div>
+</div>
+</div>
+@endsection
+@section('title') Betters @endsection
--- resources/views/horse/index.blade.php
+++ resources/views/horse/index.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.app')
 @section('content')
+
 <div class="container">
   <div class="row justify-content-center">
-    <div class="col-md-8">
+    <div class="col-md-10">
       <div class="card">
        <div class="card-header">
          <h2>Horses</h2>
+
        <form action="{{route('horse.index')}}" method="get" class="sort-form">
         <fieldset>
             <legend>Sort by: </legend>
@@ -16,17 +18,6 @@
             <div>
               <label>Runs </label>
               <input type="radio" name="sort_by" value="runs" @if('runs'==$sort) checked @endif>
-            </div>
-        </fieldset>
-        <fieldset>
-            <legend>Direction: </legend>
-            <div>
-              <label>Up </label>
-              <input type="radio" name="dir" value="asc" @if('asc'==$dir) checked @endif>
-            </div>
-            <div>
-              <label>Down </label>
-              <input type="radio" name="dir" value="desc" @if('desc'==$dir) checked @endif>
             </div>
         </fieldset>
         <button type="submit" class="btn btn-outline-dark btn-sm">Sort</button>
@@ -40,43 +31,49 @@
           </div>
       </fieldset>
       <button class="btn btn-sm btn-outline-dark my-2 my-sm-0" type="submit">Search</button>
-      <a href="{{route('horse.index')}}" class="btn btn-sm btn-outline-danger my-2 my-sm-0">Clear</a>
   </form>
     </div>
+  </div>
       
+
+    <table class="table table-striped table-dark">
+      <thead>
+        <tr>
+          <th scope="col">Photo</th>
+          <th scope="col">Name</th>
+          <th scope="col">Runs</th>
+          <th scope="col">Wins</th>
+          <th scope="col">*</th>
+            </tr>
+      </thead>
+      <tbody>
         @foreach ($horses as $horse)
-        <div class="card-body">
-          <ul class="list-group">
-            <li class="list-group-item">
-              <div class="list-container">
-                  <div class="list-container__photo">
-                  @if($horse->photo)
-                    <img src="{{$horse->photo}}">
-                    @else
-                  <img src="{{asset('no-img.png')}}">
-                  @endif
-                <div class="list-container__content">
-               {{-- <img class="card-img-top" src="..." alt="Card image cap"> --}}
-                  <span class="list-container__content__horse">Name: {{$horse->name}}<br> Runs: {{$horse->runs}}<br> Wins: {{$horse->wins}}<br></span>
-                  <span class="card-text"> About: {{$horse->about}} </span>
-                    <form method="POST" action="{{route('horse.destroy', [$horse])}}">
-                      <a href="{{route('horse.edit',$horse)}}" class="btn btn-outline-dark btn-sm">Edit</a>
-                      <button type="submit" class="btn btn-outline-danger btn-sm">DELETE</button>                      
+        <tr>
+          <td class="list-container__photo"{{$horse->photo}}>
+            @if($horse->photo)
+            <img src="{{$horse->photo}}">
+            @else
+          <img src="{{asset('no-img.png')}}">
+          @endif
+          </td>
+          <td>{{$horse->name}}</td>
+          <td>{{$horse->runs}}</td>
+          <td>{{$horse->wins}}</td>
+          <td class="list-container__buttons">
+                    <form method="POST" action="{{route('better.destroy', [$horse])}}">
                       @csrf
+                      <a href="{{route('horse.show',[$horse])}}" class="btn btn-outline-dark btn-sm">More info</a>
+                      <a href="{{route('horse.edit',[$horse])}}" class="btn btn-outline-dark btn-sm">Edit</a>
+                      <button type="submit" class="btn btn-outline-danger btn-sm">DELETE</button>
                     </form>
-                </div>
-              </div>
-              </div>
-            </div>
-          </li>
-            @endforeach
+                  </td>
+                </tr>
+                @endforeach
+                </tbody>
+              </table>
             {{$horses->links()}}
-          </ul>
-          </div>
         </div>
       </div>
     </div>
-  </div>
   @endsection
-
   @section('title') Horses @endsection
